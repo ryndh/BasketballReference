505f5b78bd03ebedf1c6762812bd57300715021b
ryanhull <ryhu23@gmail.com>
2019-01-31 10:10:18 -0700

    added try except

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
 import time
 
 driver = webdriver.Chrome('../../Selenium/chromedriver')
 driver.get('https://www.basketball-reference.com/leaders/ft_career.html')
+# driver.get('https://www.basketball-reference.com/leaders/pts_per_g_career.html')
 
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -19,22 +20,24 @@ for link in links:
 
 stats = []
 for name in names:
-    # wait = WebDriverWait(driver, 10)
-    # link = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, name)))
+    try:
+        # wait = WebDriverWait(driver, 5)
+        # link = wait.until(EC.presence_of_element_located((By.LINK_TEXT, name)))
 
-    link = driver.find_element_by_link_text(name)
-    driver.execute_script('arguments[0].click()', link)
-    # link.click()
+        link = driver.find_element_by_link_text(name)
+        # driver.execute_script('arguments[0].click()', link)
+        link.click()
 
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    table = soup.find('table', id='totals')
-    row = table.find('tfoot')
-    ftm_stat = int(row.find('td', attrs={'data-stat':'ft'}).get_text())
-    fgm_stat = int(row.find('td', attrs={'data-stat': 'fg'}).get_text())
-    stats.append(f'{name}: {fgm_stat - ftm_stat}')
-    driver.execute_script("window.history.go(-1)")
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        table = soup.find('table', id='totals')
+        row = table.find('tfoot')
+        ftm_stat = int(row.find('td', attrs={'data-stat':'ft'}).get_text())
+        fgm_stat = int(row.find('td', attrs={'data-stat': 'fg'}).get_text())
+        stats.append(f'{name}: {fgm_stat - ftm_stat}')
+        driver.execute_script("window.history.go(-1)")
+    except:
+        print(f'{name} was not able to be clicked')
 
 print(stats)
 
 driver.quit()
-
